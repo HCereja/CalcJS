03f4c83cfcda8ba153ec97aa33b046891c43e730
Henrique Costa Cereja <31112659+HCereja@users.noreply.github.com>
2023-02-15 18:40:22 -0300

    Atualizando script.js

--- script.js
+++ script.js
@@ -3,28 +3,6 @@ const main = document.querySelector("main");
 const root = document.querySelector(":root");
 const input = document.getElementById("input");
 
-const allowedKeys = [
-  "(",
-  ")",
-  "/",
-  "*",
-  "-",
-  "+",
-  "9",
-  "8",
-  "7",
-  "6",
-  "5",
-  "4",
-  "3",
-  "2",
-  "1",
-  "0",
-  ".",
-  "%",
-  " ",
-];
-
 //Controle dos botões da calculadora
 document.querySelectorAll(".charKey").forEach(function (key) {
   key.addEventListener("click", function () {
@@ -40,21 +18,6 @@ document.getElementById("clear").addEventListener("click", function () {
 });
 
 document.getElementById("equal").addEventListener("click", calculate);
-
-//Controle do teclado
-input.addEventListener("keydown", function (ev) {
-  ev.preventDefault();
-  if (allowedKeys.includes(ev.key)) {
-    input.value += ev.key;
-    return;
-  }
-  if (ev.key === "Backspace") {
-    input.value = input.value.slice(0, -1);
-  }
-  if (ev.key === "Enter") {
-    calculate();
-  }
-});
 
 //Calcular o resultado da operação
 function calculate() {
